2da321e3faef5c20cd86ddfff97781b85381dddd
RafaelRebo <rafael.reboredo@udc.es>
2024-04-03 16:53:41 +0200

    commit inicial

---
+++ cmake-build-debug/CMakeFiles/P1.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/rafael/Documentos/CP/Paralelismo-rafael.reboredo-yago.sanchezm/pi.c" "CMakeFiles/P1.dir/pi.c.o" "gcc" "CMakeFiles/P1.dir/pi.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/P1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1.dir/pi.c.o"
+  "CMakeFiles/P1.dir/pi.c.o.d"
+  "P1"
+  "P1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/P1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
